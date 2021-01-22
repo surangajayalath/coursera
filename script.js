5cd388f43ba18e69f901563c0ed0370f67be5c82
suranga <surangajayalath299@gmail.com>
2021-01-22 11:10:35 +0530

    Add existing file

---
+++ script.js
@@ -0,0 +1,11 @@
+function compute()
+{
+    p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    
+    x = p + r + y;
+    
+    
+}
+        
